975664a3a1ad29dd42b7618b19c8f287c7a87cbf
jc10101010 <95051396+jc10101010@users.noreply.github.com>
2023-03-26 23:46:10 +0100

    Update pongGameC.py

--- pongGameC.py
+++ pongGameC.py
@@ -15,7 +15,7 @@ from pygame.locals import (
 
 #Client configuration
 sendOut = b""
-host, port = "192.168.0.21", 8000
+host, port = #
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 s.sendall(b"REQUEST JOIN")
