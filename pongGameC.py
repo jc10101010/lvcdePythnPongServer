d24df9d3768faac790d21b8339b93597d9dd8964
jc10101010 <95051396+jc10101010@users.noreply.github.com>
2023-03-26 23:37:26 +0100

    Add files via upload

---
+++ pongGameC.py
@@ -0,0 +1,139 @@
+import sys
+import socket
+import pygame
+import types
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+#Client configuration
+sendOut = b""
+host, port = "192.168.0.21", 8000
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+s.sendall(b"REQUEST JOIN")
+rsp2 = s.recv(1024)
+while True:
+    rsp2 = rsp2.decode()
+    rsp = rsp2.split("\n")[0]
+    rsp = rsp.split(" ")
+    if rsp[0] == "CONFIRM" and rsp[1] == "JOIN":
+        print("CLIENT: Connection confirmed. Game started.")
+        break
+    elif rsp[0] == "WAIT":
+        print("CLIENT: Waiting in queue.")
+        rsp2 = s.recv(1024)
+    else:
+        sys.exit("CLIENT: Client detects an invalid response from the server, developer please fix.")
+s.settimeout(1/60)
+
+#Pygame configuration
+pygame.init()
+clock = pygame.time.Clock()
+WIDTH = 1000
+HEIGHT = 700
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+running = True
+
+def set_position(a, b, w, h, sw, sh):
+    a.x = b[0] - (w/2) 
+    a.y = b[1] - (h/2) + sh/2
+
+#Standard values
+paddleWidth = 20
+paddleHeight = 130
+paddleInitialDistWall = 55
+paddleSpeed = 15
+ballRadius = 15
+
+
+#Game configuration
+paddleMainPos = [paddleInitialDistWall,0]
+paddleSecondaryPos = [WIDTH - paddleInitialDistWall,0]
+ballPos = [0,0]
+paddleMain = pygame.Rect(paddleMainPos, (paddleWidth, paddleHeight))
+paddleSecondary = pygame.Rect(paddleSecondaryPos, (paddleWidth, paddleHeight))
+game_state = 1
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("CLIENT: Client quits game and ends connection.")
+            running = False
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                print("CLIENT: Client quits game and ends connection.")
+                running = False
+
+    keys_pressed = pygame.key.get_pressed()
+
+    if keys_pressed[K_UP]:
+        paddleMainPos[1] -= paddleSpeed
+    if keys_pressed[K_DOWN]:
+        paddleMainPos[1] += paddleSpeed
+
+    #Limit position
+    if paddleMainPos[1] < (-HEIGHT / 2) + paddleHeight/2:
+        paddleMainPos[1] = (-HEIGHT / 2) + paddleHeight/2
+    elif paddleMainPos[1] > (HEIGHT / 2) - paddleHeight/2:
+        paddleMainPos[1] = (HEIGHT / 2) - paddleHeight/2
+    
+    #SEND NEW INFO
+    sendOut += "SET {0},{1}\n".format( paddleMainPos[0], paddleMainPos[1]).encode()
+    sendOut += "REQUEST POS\n".encode()
+    sendOut += "REQUEST BPOS \n".encode()
+    #print("CLIENT: Client has sent information to server: \n", sendOut.decode().split("\n"))
+    
+    #READ RESPONSE
+    bytesSent = s.send(sendOut)
+    if bytesSent == len(sendOut):
+        try:
+            msg = s.recv(1024)
+            if len(msg) == 0:
+                print('CLIENT: Server has disconnected.')
+                sys.exit(0)
+            else:
+                #print("CLIENT: Client received message from server: \n", msg.decode().split())
+                #If a valid response is received:
+                lines = msg.decode().split("\n")
+                for line in lines:
+                    lns = line.split(" ")
+                    if lns[0] == "CONFIRM":
+                        if lns[1] == "POS":
+                            vals = lns[2].split(",")
+                            paddleSecondaryPos[0] = float(vals[0])
+                            paddleSecondaryPos[1] = float(vals[1])
+                        elif lns[1] == "BPOS":
+                            vals = lns[2].split(",")
+                            ballPos[0] = float(vals[0])
+                            ballPos[1] = float(vals[1])
+                        elif lns[1] == "STATE":
+                            val = int(lns[3])
+                            game_state = val
+
+                
+        except socket.timeout:
+            print("CLIENT: Server did not respond fast enough.")
+            continue
+    sendOut = b""
+
+    #Change visual positions
+    set_position(paddleMain, paddleMainPos, paddleWidth, paddleHeight, WIDTH, HEIGHT)
+    set_position(paddleSecondary, paddleSecondaryPos, paddleWidth, paddleHeight, WIDTH, HEIGHT)
+    ballDisplayPos = [ballPos[0] + WIDTH/2, ballPos[1] + HEIGHT/2]
+    #Draw objects
+    screen.fill((0, 0, 0))
+    pygame.draw.circle(screen, (255, 255, 255), ballDisplayPos, ballRadius)
+    pygame.draw.rect(screen, (255, 255, 255), paddleMain)
+    pygame.draw.rect(screen, (255, 255, 255), paddleSecondary)
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
+s.close()
---
+++ serverTest2.py
@@ -0,0 +1,235 @@
+import sys
+import socket
+import selectors
+import types
+import time
+import math
+import random
+
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print(f"SERVER: Server has accepted connection from client {addr}.")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+
+def service_connection(key, mask, paddle1, paddle2, ball, gm):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+
+        if gm.state == 0:
+            for i in range(len(gm.queue)):
+                if gm.queue[i] == data.addr:
+                    data.outb = b"CONFIRMED JOIN\n"
+                    gm.state = 1
+
+        recv_data = sock.recv(1024)
+        if recv_data:
+            cmc = recv_data.decode()
+            #print(f"SERVER: Server has received information from the client {data.addr}:  \n", cmc.split("\n"))
+            lines = cmc.split("\n")
+            for line in lines:
+                sln = line.split(" ")
+                if sln[0] == "REQUEST":
+                    if sln[1] == "JOIN":
+                        if gm.paddle1add != data.addr and gm.paddle2add != data.addr and data.addr not in gm.queue:
+                            if gm.paddle1add == "":
+                                data.outb += b"CONFIRM JOIN\n"
+                                gm.paddle1add = data.addr
+                                if gm.paddle2add != "":
+                                    gm.state = 1
+                            elif gm.paddle2add == "":
+                                data.outb += b"CONFIRM JOIN\n"
+                                gm.paddle2add = data.addr
+                                if gm.paddle1add != "":
+                                    gm.state = 1
+                            else:
+                                data.outb += b"WAIT\n"
+                                found = False
+                                for i in range(len(gm.queue)):
+                                    add = gm.queue[i]
+                                    if add == "":
+                                        found = True
+                                        gm.queue[i] = data.addr
+                                if found == False:
+                                    gm.queue.append(data.addr)
+                    elif sln[1] == "POS":
+                        if gm.paddle2add == data.addr:
+                            data.outb += "CONFIRM POS {0},{1}\n".format(-paddle1.pos[0], paddle1.pos[1]).encode()
+                        elif gm.paddle1add == data.addr:
+                            data.outb += "CONFIRM POS {0},{1}\n".format(paddle2.pos[0], paddle2.pos[1]).encode()
+                    elif sln[1] == "BPOS":
+                        if data.addr == gm.paddle1add or data.addr == gm.paddle2add:
+                            data.outb += "CONFIRM BPOS {0},{1}\n".format(ball.pos[0], ball.pos[1]).encode()
+                elif sln[0] == "SET":
+                    if sln[1] == "POS":
+                        if gm.paddle2add == data.addr:
+                            vals = sln[3].decode().split(",")
+                            paddle2.pos = [-float(vals[0]), float(vals[1])]
+                        elif gm.paddle1add == data.addr:
+                            vals = sln[3].decode().split(",")
+                            paddle1.pos = [float(vals[0]), float(vals[1])]
+        else:
+            print(f"SERVER: Closing connection to the client {data.addr}.")
+            if gm.paddle1add == data.addr:
+                gm.paddle1add = ""
+                gm.state = 0
+            elif gm.paddle2add == data.addr:
+                gm.paddle2add = ""
+                gm.state = 0
+            else:
+                for i in range(len(gm.queue)):
+                    if gm.queue[i] == data.addr:
+                        gm.queue[i] = ""
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            #print(f"SERVER: Server is sending information to the client {data.addr}: \n", (data.outb).decode().split("\n"))
+            sent = sock.send(data.outb) 
+            data.outb = data.outb[sent:]
+
+def cosd(a):
+    return math.cos(a/180 * math.pi)
+def sind(a):
+    return math.sin(a/180 * math.pi)
+   
+def seconds():
+    return time.time_ns() / 1000000000
+
+def handleWallEndCollisions(ball, gm):
+    if ball.pos[0] < (-WIDTH/2) + ball.radius - ball.radius/3:
+        gm.score += 1
+        print(gm.score)
+    elif ball.pos[0] > (WIDTH/2) - ball.radius + ball.radius/3:
+        gm.score -= 1
+        print(gm.score)
+
+def handleWallOtherCollisions(ball):
+    if ball.pos[1] < (-HEIGHT/2) + ball.radius:
+        ball.angle = (ball.angle - 90) + random.randint(-15, 15)
+    elif ball.pos[1] > (HEIGHT/2) - ball.radius:
+        ball.angle = (ball.angle - 90) + random.randint(-15, 15)
+
+def handlePaddleCollisions(ball, paddle1, paddle2):
+    ballSideLength = ball.radius * 2/3
+    c1 = False
+    c2 = False
+    #Paddle one
+    if abs(paddle1.pos[0] - ball.pos[0]) > paddle1.width/2 + ballSideLength/2:
+        c1 = True
+    if abs(paddle1.pos[1] - ball.pos[1]) > paddle1.height/2 + ballSideLength/2:
+        c2 = True
+    
+    if not c1 and not c2:
+        ball.angle = (ball.angle - 90) + random.randint(-15, 15)
+    
+    c1 = False
+    c2 = False
+    #Paddle two
+    if abs(paddle2.pos[0] - ball.pos[0]) > paddle2.width/2 + ballSideLength/2:
+        c1 = True
+    if abs(paddle2.pos[1] - ball.pos[1]) > paddle2.height/2 + ballSideLength/2:
+        c2 = True
+    
+    if not c1 and not c2:
+        ball.angle = (ball.angle - 90) + random.randint(-15, 15)
+
+    
+
+
+#Pong server setup
+WIDTH = 1000
+HEIGHT = 700
+
+class Paddle():
+    def __init__(self):
+        self.pos = [WIDTH,0]
+        self.width = 20
+        self.height = 130
+        self.distWall = 55
+        self.speed= 15
+
+
+class Ball():
+    def __init__(self):
+        self.pos = [0,0]
+        self.radius = 15
+        self.angle = 90
+        self.speed = 500
+        self.velocity = [0, self.speed]
+        self.velocityR = [self.speed, 0]
+    
+    def rotateVelocity(self):
+         a = self.angle
+         v = self.velocity
+         a2 = 360 - a
+         self.velocityR = [cosd(a2) * v[0] - sind(a2) * v[1], sind(a2) * v[0] + cosd(a2) * v[1]]
+
+class GameManager():
+    def __init__(self):
+        self.paddle1add = ""
+        self.paddle2add = ""
+        self.queue = []
+        self.state = 0
+        self.score = 0
+        self.scoreMax = 5
+        self.nextAdd = ""
+
+#Pygame setup
+paddle1 = Paddle()
+paddle2 = Paddle()
+ball = Ball()
+gm = GameManager()
+
+
+#Network setup
+sel = selectors.DefaultSelector()
+host, port = "192.168.0.21", 8000
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen()
+print(f"SERVER: Server listening on {(host, port)}.")
+s.setblocking(False)
+sel.register(s, selectors.EVENT_READ, data=None)
+
+timePrevious = seconds() 
+try:
+    while True:
+        print("\n" * 15, "PADDLE 1: ", gm.paddle1add)
+        print("PADDLE 2: ", gm.paddle2add)
+        print("QUEUE: ", gm.queue)
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask, paddle1, paddle2, ball, gm)
+        if gm.state == 1:
+            #Move ball and check for ball collisions
+            timeCurrent = seconds()
+            ball.rotateVelocity()
+            ball.pos[0] += ball.velocityR[0] * (timeCurrent - timePrevious)
+            ball.pos[1] += ball.velocityR[1] * (timeCurrent - timePrevious)
+            timePrevious = seconds()
+
+            #Paddle collisions handle
+            handlePaddleCollisions(ball, paddle1, paddle2)
+            #Wall collisions handle
+            handleWallEndCollisions(ball, gm)
+            handleWallOtherCollisions(ball)
+            
+            if abs(gm.score) == gm.scoreMax:
+                break
+        
+            
+
+        
+except KeyboardInterrupt:
+    print("SERVER: Server caught keyboard interrupt, exiting.")
+finally:
+    sel.close()
